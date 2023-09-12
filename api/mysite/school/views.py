dd29978826a3550aec180a2b95681f335e9ee5b9
wasilii <incredibleturner@gmail.com>
2023-09-13 00:36:32 +0300

    add

--- api/mysite/school/views.py
+++ api/mysite/school/views.py
@@ -3,24 +3,23 @@ from school.models import School
 from school.serializator import SchoolSerializer
 from rest_framework import status
 from rest_framework.generics import get_object_or_404
-from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 
 class SchoolListView(APIView):
     """
-    View class for listing all, and creating a new organizations, endpoint: /organization/
+    View class for listing all, and creating a new schools, endpoint: /school/
     """
     def get(self, request) -> Response:
-        """For listing out the organizations, HTTP method: GET"""
+        """For listing out the schools, HTTP method: GET"""
         schools = School.objects.all()
         # Passing the queryset through the serializer
         serializer = SchoolSerializer(schools, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
     def post(self, request) -> Response:
-        """For creating a new Organization, HTTP method: POST"""
+        """For creating a new School, HTTP method: POST"""
 
         serializer = SchoolSerializer(data=request.data)
 
@@ -32,20 +31,20 @@ class SchoolListView(APIView):
 
 class SchoolDetailView(APIView):
     """
-    View class for listing, updating and deleting a single organization,
-    endpoint: /organizations/<str:_id>/
+    View class for listing, updating and deleting a single school,
+    endpoint: /schools/<str:_id>/
     """
 
 
     def get(self, request, _id: str = None) -> Response:
-        """For listing out a single organization, HTTP method: GET"""
+        """For listing out a single school, HTTP method: GET"""
 
         school = get_object_or_404(School, pk=ObjectId(_id))
         serializer = SchoolSerializer(school)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
     def put(self, request, _id: str = None):
-        """For updating an existing organization, HTTP method: PUT"""
+        """For updating an existing school, HTTP method: PUT"""
         school = get_object_or_404(School, pk=ObjectId(_id))
         serializer = SchoolSerializer(school, data=request.data)
 
@@ -55,7 +54,7 @@ class SchoolDetailView(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request, _id: str = None) -> Response:
-        """For deleting an organization, HTTP method: DELETE"""
+        """For deleting a school, HTTP method: DELETE"""
         school = get_object_or_404(School, pk=ObjectId(_id))
         school.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
